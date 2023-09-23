c291c736705255dd185aff057a0e118daf46eaeb
eilifso <e.s.oyre@astro.uio.no>
2023-09-23 12:17:14 +0200

    [DEV] Add more -types.

--- src/initial_conditions.jl
+++ src/initial_conditions.jl
@@ -8,6 +8,8 @@
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 
+abstract type AbstractVariableIC <: AbstractInitialConditions end
+
 struct FOStaticIC{RealT<:Real} <: AbstractInitialConditions
     pos::Matrix{RealT}
     vel::Matrix{RealT}
@@ -23,6 +25,67 @@ end
 function (ic::GCAStaticIC)(i::Integer=1)
     return [ic.R[:,i]; ic.vparal[i]]
 end
+
+
+struct FO_IC <: AbstractInitialConditions
+    x ::AbstractVariableIC
+    y ::AbstractVariableIC
+    z ::AbstractVariableIC
+    vx::AbstractVariableIC
+    vy::AbstractVariableIC
+    vz::AbstractVariableIC
+end
+function (ic::FO_IC)(i::Integer=1)
+    return [ic.x(i), ic.y(i), ic.z(i), ic.vx(i), ic.vy(i), ic.vz(i)]
+end 
+
+struct GCA_IC <: AbstractInitialConditions
+    rx    ::AbstractVariableIC
+    ry    ::AbstractVariableIC
+    rz    ::AbstractVariableIC
+    vparal::AbstractVariableIC
+end
+function (ic::GCA_IC)(i::Integer=1)
+    return [ic.rx(i), ic.ry(i), ic.rz(i), ic.vparal(i)]
+end 
+
+struct FloatIC{RealT<:Real} <: AbstractVariableIC
+    float::RealT
+end
+function (ic::FloatIC)(i::Integer=1)
+    return ic.float
+end
+
+struct VectorIC{RealT<:Real} <: AbstractVariableIC
+    vec::Vector{RealT}
+end
+function (ic::VectorIC)(i::Integer=1)
+    return ic.vec[i]
+end
+
+#-------------------------------------------------------------------------------
+# On the fly generalisation of initial conditions
+#
+struct RandomUniformIC{RealT<:Real} <: AbstractVariableIC
+    init_seed::Int64
+    curr_seed::Int64
+    lower_bound::RealT
+    extent     ::RealT
+end
+function (ic::RandomUniformIC)(i::Integer=1)
+    ic.curr_seed += 1
+    r = rand(MersenneTwister(ic.curr_seed),
+             typeof(ic).parameters[1],
+             )
+    return lower_bound + extent * r
+end
+
+#struct RandomMaxwellianIC{RealT<:Real} <: AbstractVariableIC
+#    init_seed::Int64
+#    curr_seed::Int64
+#    lower_bound::
+#    extent     ::RealT
+#end
 
 
 function calc_GCA_IC_and_mu(
