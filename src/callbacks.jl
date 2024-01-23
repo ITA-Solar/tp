03f037356c65bac202b02654dbeca227474da973
eilifso <e.s.oyre@astro.uio.no>
2024-01-23 16:32:41 +0100

    [DEV] add outside-domain callback example.

--- src/callbacks.jl
+++ src/callbacks.jl
@@ -15,3 +15,20 @@ function DiscreteTPCallback()
 end
 
 
+"""
+    outside2dnullpointzoom(u, t, integrator)
+Callback condition for particles exiting the zoomed-in domain in the 2D
+nullpoint Bifrost simulation used for the results in the Japan Hinode/IRIS
+poster of septermber 2023.
+
+The effect of this callback should be termination, which means that the
+callback should be created like this:
+    affect!(integrator) = terminate!(integrator)
+    cb = DiscreteCallback(condition,affect!)
+"""
+function outside2dnullpointzoom(u,t,integrator)
+    return u[1] <= 14.52e6 || u[1] >= 18.83e6 ||
+            u[3] <= -7.79e6 || u[3] >= -3.88e6
+end
+
+
