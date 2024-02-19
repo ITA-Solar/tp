d0b30cc0442459d2a31ff15f74bd91024cc0126c
eilifso <e.s.oyre@astro.uio.no>
2024-02-19 16:34:05 +0100

    [DEV][DOC] develop and document examplerun.jl.

--- examplerun.jl
+++ examplerun.jl
@@ -1,40 +1,54 @@
 using tp
 using DifferentialEquations
+using Interpolations
 using Distributed
 
-eom = lorentzforce!
-ic = [0.0]
-p = [1.0,1.0]
-tspan = (0,1)
-npart = 1
-
-prob = ODEProblem(eom, ic, p, tspan)
-
-# For distributed parallelisation
-@everywhere function prob_func_distributed(prob, i, repeat)
-    remake(prob, u0=ic[i], p=p)
+"""
+We will in this example simulate two charged particles embedded in an
+electromagnetic field.
+"""
+eom = lorentzforce! # The equations of motion of charge particles
+                    # in a collisionless electromagntic field is the
+                    # Lorentz force.
+# Here we define the initial conditions of our two particles. The initial
+# conditions are 3 position- and 3 velocity-components, which could be
+# represented in a 6-component state vector.
+ic = [[0.0, 1.0, 0.0, 1.0, 0.0, 0.0], [0.0, 0.5, 0.0, -0.5, 0.0, 0.0]]
+# The parameters of the Lorentz equation is the particle's charge, mass and
+# functor a giving the electromagnetic fields at the particle position.
+mass = 1
+charge = [1, -1]
+struct StaticUniformElectromagneticField
+    bx::Real
+    by::Real
+    bz::Real
+    ex::Real
+    ey::Real
+    ez::Real
 end
+function (field::StaticUniformElectromagneticField)(x,y,z)
+    return [field.bx, field.by, field.bz, field.ex, field.ey, field.ez]
+end
+emfield = StaticUniformElectromagneticField(0,0,1,0,1,0)
+tspan = (0,10) # Define the simulation time-span
+npart = 2     # Define the number of particles in the simulation
+prob = ODEProblem(eom, ic[1], tspan) # Create an initial ODE-problem
+
+# Define a problem function, which will define a new ODE-problem for each
+# particle.
 function prob_func(prob, i, repeat)
-    remake(prob, u0=ic[i], p=p)
+    remake(prob, u0=ic[i], p=(mass, charge[i], emfield))
 end
-
+# Define an Ensamble-problem, to simulate the ensemble of particles
 ensamble_prob = EnsembleProblem(
-    prob, 
+    prob, # Initial problem
     prob_func=prob_func
-    ;
-    safetycopy=false,
 )
+
+# Run the simulation
 @time sim = DifferentialEquations.solve(
     ensamble_prob, 
     EnsembleSerial()
-    #EnsembleThreads()
-    #EnsembleSplitThreads()
-    #EnsembleDistributed()
     ;
-    trajectories=npart, 
-    progress=true,
-    #callback=cb,
-    maxiters=1000  
+    trajectories=npart
 )
-
-
